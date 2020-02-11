a23e7f0005dccf4caa8d8d9cb4b253e9554ace1d
Anas Majeed <anasmajeed.ch@gmail.com>
2020-02-11 23:22:32 +0500

    Scroll To Top Added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,9 +32,10 @@
 </div>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
+<script
+    src="https://code.jquery.com/jquery-3.4.1.js"
+    integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+    crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
         crossorigin="anonymous"></script>
@@ -66,6 +67,7 @@
             container_no++;
             setDisplayNone();
             setDisplayForButtons();
+            scrollToTop();
         }
     }
 
@@ -74,6 +76,7 @@
             container_no--;
             setDisplayNone();
             setDisplayForButtons();
+            scrollToTop();
         }
     }
 
@@ -93,6 +96,12 @@
             $('#nextButton').attr('style','display:block');
             $('#saveButton').attr('style','display:none');
         }
+    }
+
+    function scrollToTop() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
     }
 </script>
 @yield('scripts')
