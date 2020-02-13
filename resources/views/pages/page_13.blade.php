aed5273c641baab1fbb75378bb5dd8e6ba97897b
Anas Majeed <anasmajeed.ch@gmail.com>
2020-02-13 02:37:50 -0800

    page_13 GUI done

---
+++ resources/views/pages/page_13.blade.php
@@ -0,0 +1,187 @@
+<div id="page_13" style="display:none;">
+    <div class="col-md-12 mt-2">
+        <label for="">Documents Attached:</label>
+    </div>
+    <div class="card shadow p-3 w-100">
+        <div class="card-body ">
+            <div class="col-md-12 mt-2">
+                <label for="">Previous Passed Examination Result Card/ Degree:</label>
+            </div>
+            <div class="card shadow p-3 w-100">
+                <div class="card-body ">
+                    <div class="form-row">
+                        <div class="form-group  col-md-4">
+                            <label>Quantity Min(04):</label>
+                            <input type="text" class="form-control text-center" name="quantity"  placeholder="XXXXX">
+                        </div>
+                        <div class="form-group  col-md-4">
+                            <label>Attested by Gazzeted Officer:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row mt-4 ml-1">
+                <div class="form-group  col-md-4">
+                    <label>NOC From Previous Affiliated Body(Original):</label>
+                    <select id="districts" name="districtlist" class="form-control">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+                <div class="form-group  col-md-4">
+                    <label>Equivalence Certificate(Original):</label>
+                    <select id="districts" name="districtlist" class="form-control">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-12 mt-2">
+                <label for="">Student's Character Certificate:</label>
+            </div>
+            <div class="card shadow p-3 w-100">
+                <div class="card-body ">
+                    <div class="form-row">
+                        <div class="form-group  col-md-4">
+                            <label>Quantity Min(04):</label>
+                            <input type="text" class="form-control text-center" name="quantity"  placeholder="XXXXX">
+                        </div>
+                        <div class="form-group  col-md-4">
+                            <label>Attested by Gazzeted Officer:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row mt-4 ml-1">
+                <div class="form-group  col-md-4">
+                    <label>Student Collage Card Quantity(01):</label>
+                    <select id="districts" name="districtlist" class="form-control">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+                <div class="form-group  col-md-4">
+                    <label>Transport Card Quantity(01):</label>
+                    <select id="districts" name="districtlist" class="form-control">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-12 mt-2">
+                <label for="">Admission Offer Letter:</label>
+            </div>
+            <div class="card shadow p-3 w-100">
+                <div class="card-body ">
+                    <div class="form-row">
+                        <div class="form-group  col-md-4">
+                            <label>Original:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                        <div class="form-group  col-md-3">
+                            <label>Signed by Principal:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 mt-2">
+                <label for="">Bonafide Letter:</label>
+            </div>
+            <div class="card shadow p-3 w-100">
+                <div class="card-body ">
+                    <div class="form-row">
+                        <div class="form-group  col-md-4">
+                            <label>Original:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                        <div class="form-group  col-md-3">
+                            <label>Signed by Principal:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 mt-2">
+                <label for="">Claim Letter:</label>
+            </div>
+            <div class="card shadow p-3 w-100">
+                <div class="card-body ">
+                    <div class="form-row">
+                        <div class="form-group  col-md-4">
+                            <label>Original:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                        <div class="form-group  col-md-3">
+                            <label>Signed by Principal:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 mt-2">
+                <label for="">Rs.50/ Affidavit:</label>
+            </div>
+            <div class="card shadow p-3 w-100">
+                <div class="card-body ">
+                    <div class="form-row">
+                        <div class="form-group  col-md-3">
+                            <label>Original:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                        <div class="form-group  col-md-3">
+                            <label>Signed by Worker:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                        <div class="form-group  col-md-3">
+                            <label>Thumb Impression of Worker:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                        <div class="form-group  col-md-3">
+                            <label>Attestation by Outh Commissioner:</label>
+                            <select id="districts" name="districtlist" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
