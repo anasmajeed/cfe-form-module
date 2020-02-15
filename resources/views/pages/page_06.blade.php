dfaf50387a727d5f00eb1bd672f613c3d963aadb
Anas Majeed <anasmajeed.ch@gmail.com>
2020-02-15 19:17:48 +0500

    Page 06 Done

--- resources/views/pages/page_06.blade.php
+++ resources/views/pages/page_06.blade.php
@@ -1,94 +1,120 @@
 <div id="page_06" style="display:none;">
-    <div class="col-md-12 mt-4">
-        <label>Personal Data of Student</label>
-    </div>
-    <div class="card shadow p-3 w-100">
-        <div class="card-body ">
-            <div class="form-row">
-                <div class="form-group  col-md-6">
-                    <label>Name:</label>
-                    <input type="text" name="name" class="form-control text-center" placeholder="XXXXX" value="{{$data ? $data['student_personal_detail']['name'] : ''}}">
+    <form id="page_06_form">
+        <div class="col-md-12 mt-4">
+            <label>Personal Data of Student</label>
+        </div>
+        <div class="card shadow p-3 w-100">
+            <div class="card-body ">
+                <div class="form-row">
+                    <div class="form-group  col-md-6">
+                        <label>Name:</label>
+                        <input type="text" name="name" class="form-control text-center" placeholder="XXXXX"
+                               value="{{$data ? $data['student_personal_detail']['name'] : ''}}">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label>Father's Name:</label>
+                        <input type="text" class="form-control text-center" name="father_name" placeholder="XXXXX"
+                               value="{{$data ? $data['student_personal_detail']['father_name'] : ''}}">
+                    </div>
                 </div>
-                <div class="form-group col-md-6">
-                    <label>Father's Name:</label>
-                    <input type="text" class="form-control text-center" name="father_name" placeholder="XXXXX" value="{{$data ? $data['student_personal_detail']['father_name'] : ''}}">
+                <div class="col-md-12 mt-2">
+                    <label for="">Student's CNIC/B-Form No</label>
                 </div>
-            </div>
-            <div class="col-md-12 mt-2">
-                <label for="">Student's CNIC/B-Form No</label>
-            </div>
-            <div class="card shadow p-3 w-100">
-                <div class="card-body ">
-                    <div class="form-row">
-                        <div class="form-group col-md-4">
-                            <label>CNIC/B-Form No:</label>
-                            <input type="text" class="form-control text-center" name="cnic_no" value="{{$data ? $data['student_personal_detail']['cnic_no'] : ''}}"
-                                   placeholder="XXXXX-XXXXXXX-X">
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label>Quantity(min 04):</label>
-                            <input type="text" class="form-control text-center" name='quantity' placeholder="XXXXX" value="{{$data ? $data['student_personal_detail']['quantity'] : ''}}">
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label>Attested by Gazzeted Officer:</label>
-                            <select name="student_cnic_attested" class="form-control">
-                                <option value="yes" {{ $data ? $data['student_personal_detail']['student_cnic_attested'] == 'yes' ? 'selected' : '' : ''}}>Yes</option>
-                                <option value="no" {{ $data ? $data['student_personal_detail']['student_cnic_attested'] == 'no' ? 'selected' : '' : ''}}>No</option>
-                            </select>
+                <div class="card shadow p-3 w-100">
+                    <div class="card-body ">
+                        <div class="form-row">
+                            <div class="form-group col-md-4">
+                                <label>CNIC/B-Form No:</label>
+                                <input type="text" class="form-control text-center" name="cnic_no"
+                                       value="{{$data ? $data['student_personal_detail']['cnic_no'] : ''}}"
+                                       placeholder="XXXXX-XXXXXXX-X">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label>Quantity(min 04):</label>
+                                <input type="text" class="form-control text-center" name='quantity' placeholder="XXXXX"
+                                       value="{{$data ? $data['student_personal_detail']['quantity'] : ''}}">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label>Attested by Gazzeted Officer:</label>
+                                <select name="student_cnic_attested" class="form-control">
+                                    <option
+                                        value="yes" {{ $data ? $data['student_personal_detail']['student_cnic_attested'] == 'yes' ? 'selected' : '' : ''}}>
+                                        Yes
+                                    </option>
+                                    <option
+                                        value="no" {{ $data ? $data['student_personal_detail']['student_cnic_attested'] == 'no' ? 'selected' : '' : ''}}>
+                                        No
+                                    </option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="card shadow p-3 mt-4 w-100">
-                <div class="card-body ">
-                    <div class="form-row">
-                        <div class="form-group  col-md-3">
-                            <label>Date of Birth:</label>
-                            <input type="text" class="form-control text-center datepicker" name="date_of_birth"
-                                   placeholder="dd/mm/yyyy" value="{{$data ? $data['student_personal_detail']['date_of_birth'] : ''}}">
+                <div class="card shadow p-3 mt-4 w-100">
+                    <div class="card-body ">
+                        <div class="form-row">
+                            <div class="form-group  col-md-3">
+                                <label>Date of Birth:</label>
+                                <input type="text" class="form-control text-center datepicker" name="date_of_birth"
+                                       placeholder="dd/mm/yyyy"
+                                       value="{{$data ? $data['student_personal_detail']['date_of_birth'] : ''}}">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label>Present Address:</label>
+                                <input type="text" class="form-control text-center" name="present_address"
+                                       placeholder="XXXXX"
+                                       value="{{$data ? $data['student_personal_detail']['present_address'] : ''}}">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label>Marital Status:</label>
+                                <select name="marital_status" class="form-control">
+                                    <option value="married">Married</option>
+                                    <option value="unmarried">Unmarries</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label>Contact No 1:</label>
+                                <input type="text" class="form-control text-center" name="contact_no_1"
+                                       placeholder="+92-XXX-XXXXXXX"
+                                       value="{{$data ? $data['student_personal_detail']['contact_no_1'] : ''}}">
+                            </div>
                         </div>
-                        <div class="form-group col-md-3">
-                            <label>Present Address:</label>
-                            <input type="text" class="form-control text-center" name="present_address" placeholder="XXXXX" value="{{$data ? $data['student_personal_detail']['present_address'] : ''}}">
-                        </div>
-                        <div class="form-group col-md-3">
-                            <label>Marital Status:</label>
-                            <select  name="marital_status" class="form-control">
-                                <option value="married">Married</option>
-                                <option value="unmarried">Unmarries</option>
-                            </select>
-                        </div>
-                        <div class="form-group col-md-3">
-                            <label>Contact No 1:</label>
-                            <input type="text" class="form-control text-center" name="contact_no_1"
-                                   placeholder="+92-XXX-XXXXXXX" value="{{$data ? $data['student_personal_detail']['contact_no_1'] : ''}}">
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group  col-md-3">
-                            <label>Contact No 2:</label>
-                            <input type="text" class="form-control text-center " name="contact_no_2"
-                                   placeholder="+92-XXX-XXXXXXX" value="{{$data ? $data['student_personal_detail']['contact_no_2'] : ''}}">
-                        </div>
-                        <div class="form-group col-md-3">
-                            <label>Postal Address:</label>
-                            <input type="text" class="form-control text-center" name="postal_address" placeholder="XXXXX" value="{{$data ? $data['student_personal_detail']['postal_address'] : ''}}">
-                        </div>
-                        <div class="form-group col-md-3">
-                            <label>Email:</label>
-                            <input type="email" class="form-control text-center" name="email" value="{{$data ? $data['student_personal_detail']['email'] : ''}}"
-                                   placeholder="example@gmail.com">
-                        </div>
-                        <div class="form-group col-md-3">
-                            <label>Signature on page2(once)& 3(twice):</label>
-                            <select name="signature" class="form-control">
-                                <option value="yes"  {{ $data ? $data['student_personal_detail']['signature'] == 'yes' ? 'selected' : '' : ''}}>Yes</option>
-                                <option value="no"  {{ $data ? $data['student_personal_detail']['signature'] == 'no' ? 'selected' : '' : ''}}>No</option>
-                            </select>
+                        <div class="form-row">
+                            <div class="form-group  col-md-3">
+                                <label>Contact No 2:</label>
+                                <input type="text" class="form-control text-center " name="contact_no_2"
+                                       placeholder="+92-XXX-XXXXXXX"
+                                       value="{{$data ? $data['student_personal_detail']['contact_no_2'] : ''}}">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label>Postal Address:</label>
+                                <input type="text" class="form-control text-center" name="postal_address"
+                                       placeholder="XXXXX"
+                                       value="{{$data ? $data['student_personal_detail']['postal_address'] : ''}}">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label>Email:</label>
+                                <input type="email" class="form-control text-center" name="email"
+                                       value="{{$data ? $data['student_personal_detail']['email'] : ''}}"
+                                       placeholder="example@gmail.com">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label>Signature on page2(once)& 3(twice):</label>
+                                <select name="signature" class="form-control">
+                                    <option
+                                        value="yes" {{ $data ? $data['student_personal_detail']['signature'] == 'yes' ? 'selected' : '' : ''}}>
+                                        Yes
+                                    </option>
+                                    <option
+                                        value="no" {{ $data ? $data['student_personal_detail']['signature'] == 'no' ? 'selected' : '' : ''}}>
+                                        No
+                                    </option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </form>
 </div>
