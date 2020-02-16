a7ec857251391a4d41e5aca61b366b3e6c5a2715
Anas Majeed <anasmajeed.ch@gmail.com>
2020-02-16 02:50:06 -0800

    page_11 MC done

---
+++ app/Fields/DualCourseDetailFields.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Fields;
+
+
+class DualCourseDetailFields
+{
+    const TABLE_NAME = 'bise_details';
+    const ID = 'id';
+    const INDEX_TABLE_ID = 'index_table_id';
+    
+
+    const COURSE = 'course';
+    const ROLL_NO = 'roll_no';
+    const AFFILIATED_BODY = 'affiliated_body';
+    const DURATION_OF_COURSE = 'duration_of_course';
+    const ADMISSION_DATE = 'admission_date';
+    const ENDING_DATE = 'ending_date';
+    const SCHEME_OF_STUDY = 'scheme_of_study';
+    const SEMESTER_CATEGORY= 'semester_category';
+    const SHIFT = 'shift';
+    const REGISTRATION_STATUS = 'registration_status';
+    const REGISTRATION_DATE = 'registration_date';
+    const ACTUAL_FEE = 'actual_fee';
+    const LATE_FEE = 'late_fee';
+    const TOTAL_FEE = 'total_fee';
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+}
--- resources/views/pages/page_11.blade.php
+++ resources/views/pages/page_11.blade.php
@@ -7,60 +7,71 @@
             <div class="form-row">
                 <div class="form-group  col-md-3">
                     <label>Course:</label>
-                    <select id="districts" name="districtlist" class="form-control">
-                        <option value="allCourses">All Courses</option>
+                    <select  name="course" class="form-control">
+                        <option value="allCourses" {{ $data ? $data['dual_course_details']['course'] == 'allCourses' ? 'selected' : '' : ''}}>
+                        All Courses</option>
                     </select>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group  col-md-3">
                     <label>Roll No:</label>
-                    <input type="text" class="form-control text-center" name="roll_no" placeholder="XXXXX">
+                    <input type="text" class="form-control text-center" name="roll_no" placeholder="XXXXX"
+                    value="{{$data ? $data['dual_course_details']['roll_no'] : ''}}">
                 </div>
                 <div class="form-group  col-md-3">
                     <label>Affiliated Body:</label>
-                    <select id="districts" name="districtlist" class="form-control">
-                        <option value="xxxxx">XXXXX</option>
+                    <select  name="affiliated_body" class="form-control">
+                        <option value="xxxxx" {{ $data ? $data['dual_course_details']['affiliated_body'] == 'xxxxx' ? 'selected' : '' : ''}}>
+                        XXXXX</option>
                     </select>
                 </div>
                 <div class="form-group  col-md-3">
                     <label>Duration of Course:</label>
-                    <select id="districts" name="districtlist" class="form-control">
-                        <option value="xxxxx">XXXXX</option>
+                    <select  name="duration_of_course" class="form-control">
+                        <option value="xxxxx" {{ $data ? $data['dual_course_details']['duration_of_course'] == 'yes' ? 'selected' : '' : ''}}>
+                        XXXXX</option>
                     </select>
                 </div>
                 <div class="form-group  col-md-3">
                     <label>Date of Admission:</label>
                     <input type="text" class="form-control text-center datepicker" name="admission_date"
-                           placeholder="yyyy-mm-dd">
+                           placeholder="yyyy-mm-dd" value="{{$data ? $data['dual_course_details']['admission_date'] : ''}}">
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group  col-md-3">
                     <label>Ending Date:</label>
                     <input type="text" class="form-control text-center datepicker" name="ending_date"
-                           placeholder="yyyy-mm-dd">
+                           placeholder="yyyy-mm-dd" value="{{$data ? $data['dual_course_details']['ending_date'] : ''}}">
                 </div>
                 <div class="form-group  col-md-3">
                     <label>Scheme of Study:</label>
-                    <select id="districts" name="districtlist" class="form-control">
-                        <option value="annual">Annual</option>
-                        <option value="semester">Semester</option>
+                    <select  name="scheme_of_study" class="form-control">
+                        <option value="annual" {{ $data ? $data['dual_course_details']['scheme_of_study'] == 'annual' ? 'selected' : '' : ''}}>
+                        Annual</option>
+                        <option value="semester" {{ $data ? $data['dual_course_details']['scheme_of_study'] == 'semester' ? 'selected' : '' : ''}}>
+                        Semester</option>
                     </select>
                 </div>
                 <div class="form-group  col-md-3">
                     <label>Semester Category:</label>
-                    <select id="districts" name="districtlist" class="form-control">
-                        <option value="fall">Fall</option>
-                        <option value="spring">Spring</option>
+                    <select  name="semester_category" class="form-control">
+                        <option value="fall" {{ $data ? $data['dual_course_details']['semester_category'] == 'fall' ? 'selected' : '' : ''}}>
+                        Fall</option>
+                        <option value="spring" {{ $data ? $data['dual_course_details']['semester_category'] == 'spring' ? 'selected' : '' : ''}}>
+                        Spring</option>
                     </select>
                 </div>
                 <div class="form-group  col-md-3">
                     <label>Shift:</label>
-                    <select id="districts" name="districtlist" class="form-control">
-                        <option value="morning">Morning</option>
-                        <option value="evening">Evening</option>
-                        <option value="weekend">Weekend</option>
+                    <select  name="shift" class="form-control">
+                        <option value="morning" {{ $data ? $data['dual_course_details']['shift'] == 'morning' ? 'selected' : '' : ''}}>
+                        Morning</option>
+                        <option value="evening" {{ $data ? $data['dual_course_details']['shift'] == 'evening' ? 'selected' : '' : ''}}>
+                        Evening</option>
+                        <option value="weekend" {{ $data ? $data['dual_course_details']['shift'] == 'weekend' ? 'selected' : '' : ''}}>
+                        Weekend</option>
                     </select>
                 </div>
             </div>
@@ -69,15 +80,17 @@
                     <div class="form-row">
                         <div class="form-group col-md-4">
                             <label>Registration Status with Affiliated Body:</label>
-                            <select id="districts" name="districtlist" class="form-control">
-                                <option value="registered">Registered</option>
-                                <option value="notRegistered">Not Registered</option>
+                            <select  name="registration_status" class="form-control">
+                                <option value="registered" {{ $data ? $data['dual_course_details']['registration_status'] == 'registered' ? 'selected' : '' : ''}}>
+                                Registered</option>
+                                <option value="notRegistered" {{ $data ? $data['dual_course_details']['registration_status'] == 'notRegistered' ? 'selected' : '' : ''}}>
+                                Not Registered</option>
                             </select>
                         </div>
                         <div class="form-group col-md-3">
                             <label>Date of Registration:</label>
-                            <input type="text" class="form-control text-center datepicker" name="ending_date"
-                                   placeholder="yyyy-mm-dd">
+                            <input type="text" class="form-control text-center datepicker" name="registration_date"
+                                   placeholder="yyyy-mm-dd" value="{{$data ? $data['dual_course_details']['registration_date'] : ''}}">
                         </div>
                     </div>
                     <div class="col-md-12 mt-2">
@@ -88,18 +101,18 @@
                             <div class="form-row">
                                 <div class="form-group col-md-4">
                                     <label>Actual:</label>
-                                    <input type="text" class="form-control text-center" id="inputAddress"
-                                           placeholder="XXXXX">
+                                    <input type="text" class="form-control text-center" name="actual_fee"
+                                           placeholder="XXXXX" value="{{$data ? $data['dual_course_details']['actual_fee'] : ''}}">
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label>Late:</label>
-                                    <input type="text" class="form-control text-center " id="inputAddress"
-                                           placeholder="XXXXX">
+                                    <input type="text" class="form-control text-center " name="late_fee"
+                                           placeholder="XXXXX" value="{{$data ? $data['dual_course_details']['late_fee'] : ''}}">
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label>Total:</label>
-                                    <input type="text" class="form-control text-center" id="inputAddress"
-                                           placeholder="0000">
+                                    <input type="text" class="form-control text-center" name="total_fee"
+                                           placeholder="0000" value="{{$data ? $data['dual_course_details']['total_fee'] : ''}}">
                                 </div>
                             </div>
                         </div>
@@ -116,19 +129,19 @@
             <div class="form-row">
                 <div class="form-group col-md-3">
                     <label>Course:</label>
-                    <select id="districts" name="districtlist" class="form-control">
+                    <select  name="" class="form-control">
                         <option value="xxxxx">XXXXX</option>
                     </select>
                 </div>
                 <div class="form-group  col-md-3">
                     <label>Affiliated Body:</label>
-                    <select id="districts" name="districtlist" class="form-control">
+                    <select  name="" class="form-control">
                         <option value="xxxxx">XXXXX</option>
                     </select>
                 </div>
                 <div class="form-group  col-md-3">
                     <label>Duration of Course:</label>
-                    <select id="districts" name="districtlist" class="form-control">
+                    <select  name="" class="form-control">
                         <option value="xxxxx">XXXXX</option>
                     </select>
                 </div>
